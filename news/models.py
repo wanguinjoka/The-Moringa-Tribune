6847e3fabc20df90c21f4f658780c88264aed038
wangui.njoka <wangui.njoka@gmail.com>
2018-09-26 09:55:32 +0300

    the moringa tribune

---
+++ news/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+import datetime as dt
+
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    def save_editor(self):
+        self.save()
+
+class tags(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_tags(self):
+        self.save()
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'articles/')
+
+    @classmethod
+    def today_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date= today)
+        return news
+
+    @classmethod
+    def days_news(cls,date):
+        news = cls.objects.filter(pub_date__date = date)
+        return news
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
+        return news
